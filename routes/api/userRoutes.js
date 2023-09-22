efc31bdcd211ae0963c917dca3a853ae241e2e2e
Sarah Squyres <ssquyres15@gmail.com>
2023-09-22 14:50:15 -0500

    create user and get all users functional

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getUserByID,
+  createUser,
+  deleteUser,
+  updateUser,
+} = require('../../controllers/userControllers');
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/users/:userId
+// router.route('/:userId').get(getUserByID).put(updateUser).delete(deleteUser);
+
+module.exports = router;
