4e13a4cc257fdb0e648b14dfae895e87a084171b
Sarah Squyres <ssquyres15@gmail.com>
2023-09-22 22:43:18 -0500

    add and delete friend functional

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -62,4 +62,40 @@ module.exports = {
           res.status(500).json(err);
         }
       },
+      async addFriend(req, res) {
+        try {
+          const friend = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+          );
+    
+          if (!friend) {
+            return res.status(404).json({ message: "No user with that ID" });
+          }
+    
+          return res.status(200).json(friend);
+        } catch (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        }
+      },
+      async deleteFriend(req, res) {
+        try {
+          const friend = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+          );
+    
+          if (!friend) {
+            return res.status(404).json({ message: "Check user and friend ID" });
+          }
+    
+          return res.status(200).json(friend);
+        } catch (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        }
+      },
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,12 +5,21 @@ const {
   createUser,
   deleteUser,
   updateUser,
+  addFriend,
+  deleteFriend,
 } = require('../../controllers/userControllers');
 
 // /api/users
 router.route('/').get(getAllUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getUserByID).put(updateUser).delete(deleteUser);
+router.route('/:userId')
+.get(getUserByID)
+.put(updateUser)
+.delete(deleteUser);
+
+router.route("/:userId/friends/:friendId")
+.post(addFriend)
+.delete(deleteFriend);
 
 module.exports = router;
